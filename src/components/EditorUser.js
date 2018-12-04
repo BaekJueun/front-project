fd06770509be69022c53fea5187356371d8e7d3d
BAEK STUDIO <sosaer88@naver.com>
2018-12-04 19:07:27 +0900

    add searchList contents

--- src/components/EditorUser.js
+++ src/components/EditorUser.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react'
 
 export default class EditorUser extends Component {
+  
   render() {
     return (
     <div className="editorUserInfo">
-        <div className="editorUserIcon"></div>
-        <div className="editorUserName">Name</div>
+        <div className="editorUserIcon">{this.props.userIcon}</div>
+        <div className="editorUserName">{this.props.userName}</div>
     </div>    
     )
   }
+}
+EditorUser.defaultProps = {
+  userIcon: '',
+  userName: '홍길동'
 }
--- src/components/SearchList.js
+++ src/components/SearchList.js
@@ -6,7 +6,23 @@ export default class SearchList extends Component {
   render() {
     return (
       <div className="searchList">
-        
+        <div className="searchCard">
+          <div className="thumbnailImgame"></div>
+          <div className="">
+            <div>타이틀출력입니다.</div>
+            <div>내용출력입니다.</div>
+            <div>
+              <div>아이디출력</div>
+              <div>
+                <ul>
+                  <li>좋아요</li>
+                  <li>댓글</li>
+                  <li>링크</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
